866a0bf86d3156f23b24a2f9f6b3fa121f0d9497
sasax7 <samueltim200@yahoo.de>
2023-11-02 22:55:07 +0100

    DateWhenDatetriggered

--- de/htwg/se/TradingGame/src/main/scala/GetMarketData.scala
+++ de/htwg/se/TradingGame/src/main/scala/GetMarketData.scala
@@ -1,6 +1,6 @@
 package TradingGame
 import scala.io.Source
-
+import TradingMethods._
 object GetMarketData {
 
 def getPriceForDateTime(dateTime: String, dataFilePath: String): Double = {
@@ -28,8 +28,54 @@ def getPriceForDateTime(dateTime: String, dataFilePath: String): Double = {
   price
 }
 
-//
+//isTradeBuyorSell: true = buy, false = sell
+def isTradeBuyorSell(trade : Trade) : Boolean = {
+  if(trade.takeProfitTrade > trade.stopLossTrade){
+    true
+  } else {
+    false
+  }
+  }
+
+  
+  //dateWhenTradeTriggered: returns the date when the trade was triggered
+  //dateWhenTradeTriggered gets the Trade as an imput and goes through the data with a stream to check when the trade was triggered
+  //if the trade was triggered it returns the date when the trade was triggered
+  //if the trade was not triggered it returns "Trade was not triggered"
+  //the data is stored in a file like this: 2020.01.01,00:00,1.12345,1.12345,1.12345,1.12345,0 its Date,Time,Open,High,Low,Close,Volume
+  //get the file into a stream and check if the trade was triggered
+  //* @param trade: Trade
+  //* @return String
+  def dateWhenTradeTriggered(trade : Trade) : String = {
+    var dateWhenTradeTriggered = ""
+    val source = Source.fromFile(s"src/Symbols/${trade.ticker}.csv")
+    //put source into a stream starting from trade.date and look if trade is a buy that Low is lower or equal to trade.entryTrade or if trade is a sell that High is higher or equal to trade.entryTrade
+  
+      dateWhenTradeTriggered = source.getLines()
+        .collect {
+          case line if line.startsWith(trade.date) && isTradeBuyorSell(trade) && line.split(",")(4).toDouble <= trade.entryTrade => line.split(",")(0) // Fetching the date when the trade was triggered
+          case line if line.startsWith(trade.date) && !isTradeBuyorSell(trade) && line.split(",")(3).toDouble >= trade.entryTrade => line.split(",")(0) // Fetching the date when the trade was triggered
+        }
+        .toList
+        .lastOption
+        .getOrElse("Trade was not triggered") // If no matching line found, return "Trade was not triggered"
+        source.close()
+        dateWhenTradeTriggered
+    }
+
+    
+  }
+  
 
 
 
-}
+
+
+
+
+
+
+
+
+
+
--- de/htwg/se/TradingGame/src/test/scala/GetMarketDataSpec.scala
+++ de/htwg/se/TradingGame/src/test/scala/GetMarketDataSpec.scala
@@ -6,6 +6,21 @@ import GetMarketData._
 
 class GetMarketDataSpec extends AnyWordSpec with Matchers {
 
+"isTradeBuyorSell" should {
+  "return true if the trade is a buy" in {
+    val trade = Trade(1.00181 , 1.00015, 1.00250, 2.0, "2022.09.08,15:00", "EURUSD")
+    val result = isTradeBuyorSell(trade)
+    result shouldEqual true
+  }
+}
+
+  "return false if the trade is a sell" in {
+    val trade = Trade(1.00181 , 1.00250, 1.00015, 2.0, "2022.09.08,15:00", "EURUSD")
+    val result = isTradeBuyorSell(trade)
+    result shouldEqual false
+  }
+
+
   "getPriceForDateTime" should {
     "return the correct price for a given date and time" in {
       // Create a temporary test file for the test
@@ -48,6 +63,46 @@ class GetMarketDataSpec extends AnyWordSpec with Matchers {
     }
   }
 
+"dateWhenTradeTriggered " should {
+  "return the correct date when the trade was triggered" in {
+    // Create a temporary test file for the test
+    val testFilePath = "testFile.csv"
+    val writer = new PrintWriter(new File(testFilePath))
+    writer.write(
+      """2022.09.08,15:00,1.00198,1.00218,1.00182,1.00202,155
+        |2022.09.08,15:11,1.00176,1.00208,1.00176,1.00208,132
+        |2022.09.08,15:34,0.99954,1.00015,0.99954,1.00002,308""".stripMargin)
+    writer.close()
 
+    // Test scenario
+    val result = dateWhenTradeTriggered(Trade(1.00181 , 1.00015, 1.00250, 2.0, "2022.09.08,15:00", "EURUSD"))
+
+    // Assertion
+    result shouldEqual "2022.09.08:15:11"
+
+    // Clean up: Delete the temporary file
+    val file = new File(testFilePath)
+    if (file.exists()) file.delete()
+  }
+
+  "return Trade was not triggered when no matching date and time are found" in {
+    val testFilePath = "testFileEmpty.csv"
+    val writer = new PrintWriter(new File(testFilePath))
+    writer.write(
+      """2022.09.08,15:15,1.00096,1.00300,0.99998,1.00005,435
+        |2022.09.08,15:28,1.00074,1.00103,1.00049,1.00059,178""".stripMargin)
+    writer.close()
+
+    // Test scenario
+    val result = dateWhenTradeTriggered(Trade(0.995 , 0.8, 1.00250, 2.0, "2022.08.17,23:51", "EURUSD"))
+
+    // Assertion
+    result shouldEqual "Trade was not triggered"
+
+    // Clean up: Delete the temporary file
+    val file = new File(testFilePath)
+    if (file.exists()) file.delete()
   
+}
+}
 }
