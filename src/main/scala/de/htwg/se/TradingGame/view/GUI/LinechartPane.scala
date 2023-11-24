615b7ab97f7df8058c2842d72ce3de64b946da63
sasax7 <samueltim200@yahoo.de>
2023-11-24 12:34:32 +0100

    1

--- src/main/scala/de/htwg/se/TradingGame/view/GUI/LinechartPane.scala
+++ src/main/scala/de/htwg/se/TradingGame/view/GUI/LinechartPane.scala
@@ -28,8 +28,6 @@ class LinechartPane extends StackPane {
     lineChart.setCreateSymbols(false)
     lineChart.setLegendVisible(false)
 
-
-
     crosshairVerticalLine.setStrokeWidth(1)
     crosshairVerticalLine.getStrokeDashArray.addAll(5d, 5d)
     crosshairVerticalLine.setStartY(0)
@@ -43,105 +41,13 @@ class LinechartPane extends StackPane {
     val chartPane = new StackPane()
     chartPane.getChildren.addAll(lineChart, crosshairVerticalLine, crosshairHorizontalLine)
 
-
-    // Set up event handlers for mouse interactions
-    onMousePressed = (event: MouseEvent) => {
-      val point = sceneToLocal(new Point2D(event.sceneX, event.sceneY))
-      startX = point.getX
-      startY = point.getY
-    }
-
-    onMouseDragged = (event: MouseEvent) => {
-      val point = chartPane.sceneToLocal(new Point2D(event.sceneX, event.sceneY))
-
-      if (point.getX > lineChart.width.value - 50) {
-        // Zooming in or out
-        val zoomFactor = 0.01 // Adjust this value to control the zoom speed
-        val dragY = point.getY - startY
-        val yAxisLowerBound = yAxis.getLowerBound
-        val yAxisUpperBound = yAxis.getUpperBound
-        val range = yAxisUpperBound - yAxisLowerBound
-        val zoomStep = range * zoomFactor
-
-        if (dragY < 0) {
-          // Dragging up, show lower numbers
-          val newLowerBound = yAxisLowerBound + zoomStep
-          val newUpperBound = yAxisUpperBound - zoomStep
-          yAxis.setLowerBound(newLowerBound)
-          yAxis.setUpperBound(newUpperBound)
-        } else {
-          // Dragging down, show higher numbers
-          val newLowerBound = yAxisLowerBound - zoomStep
-          val newUpperBound = yAxisUpperBound + zoomStep
-          yAxis.setLowerBound(newLowerBound)
-          yAxis.setUpperBound(newUpperBound)
-        }
-      } else {
-        // Panning
-        val yAxisHeight = yAxis.getHeight
-        val yAxisRange = yAxis.getUpperBound - yAxis.getLowerBound
-        val pixelToYAxisUnitRatio = yAxisRange / yAxisHeight
-
-        val yDiffPixels = point.getY - startY
-        val yDiff = yDiffPixels * pixelToYAxisUnitRatio
-
-        val yAxisLowerBound = yAxis.getLowerBound
-        val yAxisUpperBound = yAxis.getUpperBound
-
-        yAxis.setLowerBound(yAxisLowerBound + yDiff)
-        yAxis.setUpperBound(yAxisUpperBound + yDiff)
-
-        val xAxisWidth = xAxis.getWidth
-        val xAxisRange = xAxis.getUpperBound - xAxis.getLowerBound
-        val pixelToXAxisUnitRatio = xAxisRange / xAxisWidth
-
-        val xDiffPixels = point.getX - startX
-        val xDiff = xDiffPixels * pixelToXAxisUnitRatio
-
-        val xAxisLowerBound = xAxis.getLowerBound
-        val xAxisUpperBound = xAxis.getUpperBound
-
-        xAxis.delegate.setLowerBound(xAxisLowerBound - xDiff)
-        xAxis.delegate.setUpperBound(xAxisUpperBound - xDiff)
-
-        crosshairVerticalLine.setStartX(crosshairVerticalLine.getStartX + xDiffPixels)
-        crosshairVerticalLine.setEndX(crosshairVerticalLine.getEndX + xDiffPixels)
-        crosshairHorizontalLine.setStartY(crosshairHorizontalLine.getStartY + yDiffPixels)
-        crosshairHorizontalLine.setEndY(crosshairHorizontalLine.getEndY + yDiffPixels)
-      }
-
-      updateYAxis()
-      startX = point.getX
-      startY = point.getY
-    }
-
-    lineChart.onScroll = (event: ScrollEvent) => {
-      val zoomFactor = 0.1 // Adjust this value to control the zoom speed
-
-      val deltaY = event.getDeltaY
-      val xAxisLowerBound = xAxis.getLowerBound
-      val xAxisUpperBound = xAxis.getUpperBound
-      val range = xAxisUpperBound - xAxisLowerBound
-      val zoomStep = range * zoomFactor
-      val xAxisWidth = xAxis.getWidth
-      val zoomhighlowlines = xAxisWidth * zoomFactor
-
-      if (deltaY < 0) {
-        // Zoom out
-        val newLowerBound = xAxisLowerBound - zoomStep
-        xAxis.setLowerBound(newLowerBound)
-      } else {
-        // Zoom in
-        val newLowerBound = xAxisLowerBound + zoomStep
-        xAxis.setLowerBound(newLowerBound)
-      }
-    }
-
-    
-
     children.addAll(chartPane)
 
     updateYAxis()
+
+    // Add event handlers for mouse enter and exit
+    lineChart.onMouseEntered = (event: MouseEvent) => showCrosshair()
+    lineChart.onMouseExited = (event: MouseEvent) => hideCrosshair()
   }
 
   private def updateYAxis(): Unit = {
@@ -153,5 +59,15 @@ class LinechartPane extends StackPane {
     val tickUnit = range / tickCount
 
     yAxis.setTickUnit(tickUnit)
+  }
+
+  private def showCrosshair(): Unit = {
+    crosshairVerticalLine.setVisible(true)
+    crosshairHorizontalLine.setVisible(true)
+  }
+
+  private def hideCrosshair(): Unit = {
+    crosshairVerticalLine.setVisible(false)
+    crosshairHorizontalLine.setVisible(false)
   }
 }
