b6480d779a6406bf4f03b728b7b403df83ea906e
sasax7 <samueltim200@yahoo.de>
2023-12-01 13:01:07 +0100

    gui Ticker selection and timeframe selection

--- src/main/scala/de/htwg/se/TradingGame/view/GUI/BacktestStage.scala
+++ src/main/scala/de/htwg/se/TradingGame/view/GUI/BacktestStage.scala
@@ -25,6 +25,11 @@ import scalafx.scene.layout.Priority
 import scalafx.scene.layout.Region
 import scalafx.scene.layout.StackPane
 import scalafx.scene.layout.VBox
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.control.ComboBox
+import scalafx.scene.control.DatePicker
+import scalafx.scene.control.Spinner
+import scalafx.scene.control.SpinnerValueFactory.IntegerSpinnerValueFactory
 
 object BacktestStage extends JFXApp3 {
     override def start(): Unit = {
@@ -32,10 +37,27 @@ object BacktestStage extends JFXApp3 {
     }
     val crosshairPane = new Pane()
     def createStage(): PrimaryStage = {
+        val timeframeOptions = ObservableBuffer("1m", "5m", "15m", "1h", "4h", "1day", "1week")
+        val timeframeComboBox = new ComboBox[String](timeframeOptions)
+        timeframeComboBox.value = "1m"
+        val tickerDropdown = new TickerSelection()
+        val tickerComboBox = tickerDropdown.createTickerDropdown()
         val endButton = new Button("Finish Backtesting")
         val Button1 = new Button("Button1")
         val Button2 = new Button("Button2")
-        val topButtons = new HBox(endButton, Button1, Button2)
+        val topButtons = new HBox(endButton, tickerComboBox,timeframeComboBox, Button1, Button2)
+
+        val dateInput = new DatePicker()
+        val dateLabel = new Label("Date: ")
+        val dateBox = new HBox(dateLabel, dateInput)
+        val hourSpinner = new Spinner[Int](0, 23, 0)
+        val minuteSpinner = new Spinner[Int](0, 59, 0)
+
+        val hourLabel = new Label("Hour: ")
+        val minuteLabel = new Label("Minute: ")
+
+        val hourBox = new HBox(hourLabel, hourSpinner)
+        val minuteBox = new HBox(minuteLabel, minuteSpinner)
 
         val leftButtons = new VBox(
             new Button("Button 1"),
@@ -52,22 +74,24 @@ object BacktestStage extends JFXApp3 {
 
  
         val chartpane = new LinechartPane()
-        chartpane.initializeLineChart()
+        chartpane.initializeLineChart(tickerComboBox.value.value)
         VBox.setVgrow(chartpane, Priority.Always)
 
-        
-       
-
-       
 
         val chartWithCrosshair = new ChartDragHandler(chartpane, crosshairPane)
-
-
-
-
-
-
+        val applyDateButton = new Button("Apply Date")
+        applyDateButton.setOnAction(_ => {
+        val selectedDate = dateInput.getValue // Get the selected date from the date picker
+        val selectedHour = hourSpinner.getValue // Get the selected hour from the spinner
+        val selectedMinute = minuteSpinner.getValue // Get the selected minute from the spinner
+        val dateTime = selectedDate.atTime(selectedHour, selectedMinute) // Combine the date and time into a LocalDateTime object
+        chartpane.updateDate(dateTime)
+        })
         val inputBox = new VBox(
+            dateBox,
+            hourBox,
+            minuteBox,
+            applyDateButton,
             new Label("Input 1"),
             new TextField(),
             new Label("Input 2"),
@@ -93,9 +117,6 @@ object BacktestStage extends JFXApp3 {
         splitPane1.orientation = Orientation.Vertical
         splitPane1.items.addAll(splitPane2, tablewithlabel)
 
-
-        
-
         val horizontalBox = new HBox(leftButtons, splitPane1, rightButtons)
         HBox.setHgrow(splitPane1, Priority.Always)
 
@@ -114,6 +135,15 @@ object BacktestStage extends JFXApp3 {
             stage.hide()
             BacktestEvaluation.createStage().show()
         })
+        tickerComboBox.value.onChange { (_, _, newTicker) =>
+            chartpane.initializeLineChart(newTicker)
+            }
+
+        timeframeComboBox.onAction = () => {
+            val selectedTimeframe = timeframeComboBox.value.value
+            // Update the line chart with the selected timeframe
+            chartpane.updateTimeframe(selectedTimeframe)
+            }
 
         stage
     }
---
+++ src/main/scala/de/htwg/se/TradingGame/view/GUI/TickerSelection.scala
@@ -0,0 +1,21 @@
+package de.htwg.se.TradingGame.view.GUI
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.control.ComboBox
+import java.io.File
+
+class TickerSelection {
+  def createTickerDropdown(): ComboBox[String] = {
+    val Path: String = new File("src/main/scala/de/htwg/se/TradingGame/model/BrowseInterpreter.scala").getAbsolutePath
+    val file = new File(Path).getParent + s"/Symbols"
+    val folder = new File(file)
+    val csvFiles = folder.listFiles.filter(_.getName.endsWith(".csv"))
+    val tickerOptions = ObservableBuffer(csvFiles.map(_.getName.stripSuffix(".csv")): _*)
+    val tickerDropdown = new ComboBox[String](tickerOptions)
+    tickerDropdown.promptText = "Select Ticker"
+    if (tickerOptions.nonEmpty) {
+      tickerDropdown.value = tickerOptions.head
+    }
+    
+    tickerDropdown
+  }
+}
