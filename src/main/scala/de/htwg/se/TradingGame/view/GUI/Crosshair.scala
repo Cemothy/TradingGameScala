11c6c5f4fdb51b0b79903a6dda4c9108c66aa065
sasax7 <samueltim200@yahoo.de>
2023-12-01 01:27:53 +0100

    Gui working chart

---
+++ src/main/scala/de/htwg/se/TradingGame/view/GUI/Crosshair.scala
@@ -0,0 +1,39 @@
+package de.htwg.se.TradingGame.view.GUI
+
+import scalafx.scene.input.MouseEvent
+import scalafx.scene.layout.Pane
+import scalafx.scene.shape.Line
+
+class Crosshair(chartpane: Pane) {
+  // Create two Line objects for the crosshair
+  val crosshairVertical = new Line()
+  val crosshairHorizontal = new Line()
+
+  def createCrosshair(): Unit = {
+    // Add the crosshair lines to the chart
+    chartpane.children.addAll(crosshairVertical, crosshairHorizontal)
+
+    // Set initial properties for the crosshair lines
+    crosshairVertical.setStrokeWidth(0.5)
+    crosshairVertical.strokeDashArray = Seq(5d, 5d)
+    crosshairVertical.mouseTransparent = true
+
+
+    crosshairHorizontal.setStrokeWidth(0.5)
+    crosshairHorizontal.strokeDashArray = Seq(5d, 5d)
+    crosshairHorizontal.mouseTransparent = true
+  }
+
+  def updateCrosshair(me: MouseEvent): Unit = {
+    // Update the position of the crosshair lines based on the mouse position
+    crosshairVertical.setStartX(me.getX)
+    crosshairVertical.setEndX(me.getX)
+    crosshairVertical.setStartY(0)
+    crosshairVertical.setEndY(chartpane.height.value)
+
+    crosshairHorizontal.setStartY(me.getY)
+    crosshairHorizontal.setEndY(me.getY)
+    crosshairHorizontal.setStartX(0)
+    crosshairHorizontal.setEndX(chartpane.width.value)
+  }
+}
