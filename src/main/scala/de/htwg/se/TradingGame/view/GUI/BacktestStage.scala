e805eff862be8d53417b79ea375904b8dc476fe9
sasax7 <samueltim200@yahoo.de>
2023-12-19 00:17:47 +0100

    faster play with slider

--- src/main/scala/de/htwg/se/TradingGame/view/GUI/BacktestStage.scala
+++ src/main/scala/de/htwg/se/TradingGame/view/GUI/BacktestStage.scala
@@ -9,11 +9,7 @@ import de.htwg.se.TradingGame.model.TradeDecoratorPattern.TradeComponent
 import de.htwg.se.TradingGame.model.TradeDecoratorPattern.TradeDoneCalculations
 import de.htwg.se.TradingGame.model.TradeDecoratorPattern.TradeWithVolume
 import de.htwg.se.TradingGame.model.TradeDecoratorPattern.TradeisBuy
-import de.htwg.se.TradingGame.view.GUI.AdvCandleStickChartSample.addData
-import de.htwg.se.TradingGame.view.GUI.AdvCandleStickChartSample.clearAndAddData
-import de.htwg.se.TradingGame.view.GUI.AdvCandleStickChartSample.createChart
-import de.htwg.se.TradingGame.view.GUI.AdvCandleStickChartSample.deleteFirstCandle
-import de.htwg.se.TradingGame.view.GUI.AdvCandleStickChartSample.updateCandleStickChartAxis
+import de.htwg.se.TradingGame.view.GUI.AdvCandleStickChartSample._
 import de.htwg.se.TradingGame.view.GUI.BalanceStage
 import de.htwg.se.TradingGame.view.GUI.GetAPIData._
 import javafx.event.ActionEvent
@@ -57,11 +53,16 @@ import scalafx.scene.layout.Region
 import scalafx.scene.layout.StackPane
 import scalafx.scene.layout.VBox
 import scalafx.scene.paint.Color
-
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.time.OffsetDateTime
+import scalafx.animation.{KeyFrame, Timeline}
+import scalafx.util.Duration
+import javafx.event.{ActionEvent, EventHandler}
+import scalafx.scene.control.Slider
+import java.time.ZoneOffset
 
 object BacktestStage extends JFXApp3 {
     val controller = new Controller()
@@ -288,8 +289,52 @@ class BacktestStage(controller: Controller){
         }
         val endButton = new Button("Finish Backtesting")
         val button1 = new Button("<<")
+        
         val button2 = new Button(">>")
         val profitLabel = new Label(s"Profit: $summprofit       ")
+        val speedSlider = new Slider {
+            min = 10
+            max = 1000
+            value = 200
+        }
+
+        class TimelineToggleButton extends ToggleButton {
+            var timeline: Timeline = _
+
+            def startTimeline(): Unit = {
+                timeline = new Timeline {
+                    cycleCount = Timeline.Indefinite
+                    keyFrames = KeyFrame(Duration(speedSlider.value.value), onFinished = _ => showData(chart))
+                }
+                timeline.play()
+            }
+        }
+
+        val runDataButton = new TimelineToggleButton {
+            text = "Run Data"
+
+            selected.onChange { (_, _, isSelected) =>
+                if (isSelected) {
+                    startTimeline()
+                    text = "Stop"
+                } else {
+                    if (timeline != null) {
+                        timeline.stop()
+                    }
+                    text = "Run Data"
+                }
+            }
+        }
+
+        speedSlider.value.onChange { (_, _, newValue) =>
+            if (runDataButton.selected.value) {
+                if (runDataButton.timeline != null) {
+                    runDataButton.timeline.stop()
+                }
+                runDataButton.startTimeline()
+            }
+        }
+
         button2.onAction = () => {
             val currentDateTime = LocalDateTime.parse(dateInput.text.value, formatter)
 
@@ -306,8 +351,8 @@ class BacktestStage(controller: Controller){
 
             dateInput.text.value = newDateTime.format(formatter)
 
-            val lastCandleStick = GetAPIData.getLastCandleStick(timeframeComboBox.value.value, tickerComboBox.text.value, newDateTime)
-            addData(chart, lastCandleStick)
+            
+            showData(chart)
 
             tradesBuffer.clear()
             tradesBuffer ++= GetMarketData.donetrades.map(trade => {
@@ -350,7 +395,7 @@ class BacktestStage(controller: Controller){
             table.refresh()
         }
         
-        val topButtons = new HBox(endButton, tickerComboBox,timeframeComboBox, button1, button2)
+        val topButtons = new HBox(endButton, tickerComboBox,timeframeComboBox, button1,runDataButton, button2, speedSlider)
 
        
 
@@ -455,8 +500,9 @@ class BacktestStage(controller: Controller){
                 val dateString = localDateTime.format(formatter)
                 dateInput.text = dateString
                 val newdata = getCandleSticks(timeframeComboBox.value.value, tickerComboBox.text.value, LocalDateTime.parse(dateInput.text.value, formatter))
-                clearAndAddData(chart, newdata)
+                addDataAndHideAfterDate(chart, newdata, localDateTime.atZone(ZoneId.systemDefault()).toEpochSecond)
                 //updateCandleStickChartAxis(chart, newdata)
+            
                 nextClickAction = ""
             }
         }
