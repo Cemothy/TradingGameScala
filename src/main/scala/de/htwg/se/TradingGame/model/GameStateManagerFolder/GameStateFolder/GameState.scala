36272d1497f25220e35e7111890dbd5725cfaea9
sasax7 <samueltim200@yahoo.de>
2024-01-23 16:26:16 +0100

    FileIO test

--- src/main/scala/de/htwg/se/TradingGame/model/GameStateManagerFolder/GameStateFolder/GameState.scala
+++ src/main/scala/de/htwg/se/TradingGame/model/GameStateManagerFolder/GameStateFolder/GameState.scala
@@ -3,45 +3,63 @@ import com.google.inject.Guice
 import com.google.inject.Inject
 import com.google.inject.Injector
 import com.google.inject.name.Names
-import de.htwg.se.TradingGame.model.TradeDecoratorPattern.TradeComponent
 import de.htwg.se.TradingGame.model.TradeDecoratorPattern.Decorator.ConcreteDecorators._
+import de.htwg.se.TradingGame.model.TradeDecoratorPattern.TradeComponent
 
 import java.util.concurrent.CopyOnWriteArrayList
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.ListBuffer
 import scala.jdk.CollectionConverters._
 trait GameState {
-    def balance: Double
-    def backtestDate: Long
-    def trades: ArrayBuffer[TradeComponent]
-    def doneTrades: ArrayBuffer[TradeDoneCalculations]
-    def startbalance: Double
-    def pair: String
-    def savename: String
-    def endDate: Long
-    def startDate: Long
-    def databaseConnectionString: String
-    def pairList: List[String]
-    def distancecandles: Int
-    def interval: String
-    def loadFileList: List[String]
-    override def toString: String = {
-    s"""
-      |Balance: $balance
-      |Backtest Date: $backtestDate
-      |Trades: $trades
-      |Done Trades: $doneTrades
-      |Start Balance: $startbalance
-      |Pair: $pair
-      |Save Name: $savename
-      |End Date: $endDate
-      |Start Date: $startDate
-      |Database Connection String: $databaseConnectionString
-      |Pair List: $pairList
-      |Distance Candles: $distancecandles
-      |Interval: $interval
-      |Load File List: $loadFileList
-    """.stripMargin
+  def balance: Double
+  def backtestDate: Long
+  def trades: ArrayBuffer[TradeComponent]
+  def doneTrades: ArrayBuffer[TradeDoneCalculations]
+  def startbalance: Double
+  def pair: String
+  def savename: String
+  def endDate: Long
+  def startDate: Long
+  def databaseConnectionString: String
+  def pairList: List[String]
+  def distancecandles: Int
+  def interval: String
+  def loadFileList: List[String]
+  override def toString: String = {
+  s"""
+    |Balance: $balance
+    |Backtest Date: $backtestDate
+    |Trades: $trades
+    |Done Trades: $doneTrades
+    |Start Balance: $startbalance
+    |Pair: $pair
+    |Save Name: $savename
+    |End Date: $endDate
+    |Start Date: $startDate
+    |Database Connection String: $databaseConnectionString
+    |Pair List: $pairList
+    |Distance Candles: $distancecandles
+    |Interval: $interval
+    |Load File List: $loadFileList
+  """.stripMargin
+  }
+  override def equals(obj: Any): Boolean = obj match {
+    case other: GameState =>
+      balance == other.balance &&
+      backtestDate == other.backtestDate &&
+      trades == other.trades &&
+      doneTrades == other.doneTrades &&
+      startbalance == other.startbalance &&
+      pair == other.pair &&
+      savename == other.savename &&
+      endDate == other.endDate &&
+      startDate == other.startDate &&
+      databaseConnectionString == other.databaseConnectionString &&
+      pairList == other.pairList &&
+      distancecandles == other.distancecandles &&
+      interval == other.interval &&
+      loadFileList == other.loadFileList
+    case _ => false
   }
 }
 
